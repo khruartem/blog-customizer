ba378d9ea3399c11b30d6c7ddc34d32614f5deb2
Artem Khrustalev <khru-artem@yandex.ru>
2024-11-16 18:55:26 +0300

    исправление замечаний 'можно лучше'

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import {
-	StrictMode,
-	CSSProperties,
-	useState,
-	useRef,
-	SyntheticEvent,
-} from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	backgroundColors,
-	contentWidthArr,
-	defaultArticleState,
-	fontFamilyOptions,
-	fontSizeOptions,
-	OptionType,
-	SelectedOption,
-	fontColors,
-	ArticleStateType,
-} from './constants/articleProps';
-import { Select } from './components/select/Select';
+import { App } from './components/app/App';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
-import { Separator } from './components/separator';
-import { Button } from './components/button';
-import { RadioGroup } from './components/radio-group';
+import { StrictMode } from 'react';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [appStyle, setAppStyle] = useState(defaultArticleState);
-	
-	return (
-		<div
-			className={styles.main}
-			style={
-				{
-					'--font-family': appStyle.fontFamilyOption.value,
-					'--font-size': appStyle.fontSizeOption.value,
-					'--font-color': appStyle.fontColor.value,
-					'--container-width': appStyle.contentWidth.value,
-					'--bg-color': appStyle.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm appStyle={appStyle} changeAppStyle={setAppStyle}/>
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
